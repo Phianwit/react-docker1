d29efc8e9e39416205a16a641fc3ebef453bc397
sirawasitjintaphattharakul <sirawasitjintaphattharakul@gmail.com>
2024-10-11 13:23:15 +0700

    button

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -27,7 +27,7 @@ export default function MyCard(props) {
   </CardContent>
   <CardActions>
     <Button size="small">Learn More</Button>
-    <Button size="small">v2</Button>
+    <Button size="small">v3</Button>
   </CardActions>
 </Card>
 
